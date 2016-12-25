392a067052db7e1ef77cf54a9899484e43cb7779
JurrijnP <slubadog@gmail.com>
2016-12-25 20:53:21 +0100

    Added style
    Used Stylesheet from Echo Webmanager

--- Home.php
+++ Home.php
@@ -4,6 +4,7 @@
 	<head>
 		<title>A.R.S. Echo Creator</title>
 		<link rel="icon" href="https://cdn.discordapp.com/app-icons/249891250117804032/8006c62af129d35355732de574da495c.jpg" type="image/x-icon">
+		<link rel="stylesheet" href="https://webm.xtclabs.net/css/application.min.css" type="text/css">
 	</head>
 	
 	<body>
