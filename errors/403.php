af3954c11a2e79cd5d76d959a4f55ccc8b815c9b
JurrijnP <slubadog@gmail.com>
2016-12-25 23:10:54 +0100

    Added errors
    Added Error 403 - Acces Denied

---
+++ errors/403.php
@@ -0,0 +1,125 @@
+<!DOCTYPE html>
+<html lang=en_US>
+	<head>
+		<title>No Access!</title>
+		<link href="css/403.css" type="text/css" rel="stylesheet">
+		<link rel="icon" href="https://cdn.discordapp.com/app-icons/249891250117804032/8006c62af129d35355732de574da495c.jpg" type="image/x-icon">
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+		<style>
+			* {
+	margin: 0;
+	padding: 0;
+}
+
+body {
+	text-align: center;
+	font-family: Arial;
+	background:url("../images/stripe.png") repeat;
+}
+
+.bgclr{
+    background-color: #154265 20%;
+    background-image: radial-gradient(farthest-side ellipse at 10% 0, #154265 20%, #7392c2);
+    background-image: -webkit-radial-gradient(10% 0, farthest-side ellipse, #154265 20%, #7392c2);
+    background-image: -moz-radial-gradient(10% 0, farthest-side ellipse, #154265 20%, #7392c2);
+    background-attachment: fixed, fixed;
+position: absolute;
+width: 100%;
+min-height: 100%;
+}
+
+#page-wrap {
+	border-radius: 15px;
+	width: 720px;
+	min-width: 700px;
+	background: rgba(51,51,51,0.4);
+	padding: 20px 50px 20px 50px;
+	margin: 50px auto;
+	height: 400px;
+	z-index:10000;
+}
+
+.title {
+	text-align: center;
+	font-weight: bold;
+	text-transform: uppercase;
+	font-size: 75px;
+}
+
+.text {
+	width: 100%;
+	margin: 1px;
+	font-family: Calibri;
+	font-size: 20px;
+}
+
+button {
+	border-radius: 9px;
+    background: #cb002f;
+    color: #ffffff;
+	font-weight: bold;
+	font-size: 18px;
+	padding: 10px;
+    text-align: center;
+    border: 0;
+	-webkit-transition: background-color 2s ease-out;
+	-moz-transition: background-color 2s ease-out;
+	-o-transition: background-color 2s ease-out;
+	transition: background-color 2s ease-out;
+    transition: all 0.5s ease 0s;
+}
+
+button:hover {
+	background: #af0028;
+}
+
+input.goback {
+	border-radius: 10px;
+    background: #cb002f;
+    color: #ffffff;
+	font-weight: bold;
+	font-size: 20px;
+	padding: 10px;
+    text-align: center;
+    border: 0;
+	-webkit-transition: background-color 2s ease-out;
+	-moz-transition: background-color 2s ease-out;
+	-o-transition: background-color 2s ease-out;
+	transition: background-color 2s ease-out;
+    transition: all 0.5s ease 0s;
+}
+
+input.goback:hover {
+	background: #af0028;
+}
+
+a {
+	color: #ffffff;
+	text-decoration: none;
+}
+
+a:hover {
+	color: #ffffff;
+	text-decoration: none;
+}
+
+a:link, a:visited {
+	color: #ffffff;
+	text-decoration: none;
+}
+
+		</style>
+	</head>
+	
+	<body>
+		<div class="bgclr">
+			<div id="page-wrap">
+				<h1 class="title">Not so fast.</h1>
+				<h1 class="title">ಠ╭╮ಠ</h1><br><br>
+				<p class="text">Looks like you are trying to enter a page that isn't accessable for you.</p>
+				<br><br><br><br>
+				<button><a href="Contact">Contact us if you think this is not right.</a></button><button style="margin-left: 5px;"><a href="../Home">Go back where you came from.</a></button>
+			</div>
+		</div>
+	</body>
+</html>
