21f503ebdb957d5080c06b26976c4a4df7541c88
JurrijnP <slubadog@gmail.com>
2016-12-25 23:40:08 +0100

    More error codes.
    Added Error 404 - Not Found. Updated Error 403 - Access Denied. Added
    overall css for Error pages.

--- errors/404.php
+++ errors/404.php
@@ -2,7 +2,7 @@
 <html lang=en_US>
 	<head>
 		<title>No Access!</title>
-		<link href="css/403.css" type="text/css" rel="stylesheet">
+		<link href="css/errors.css" type="text/css" rel="stylesheet">
 		<link rel="icon" href="https://cdn.discordapp.com/app-icons/249891250117804032/8006c62af129d35355732de574da495c.jpg" type="image/x-icon">
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 	</head>
@@ -10,11 +10,11 @@
 	<body>
 		<div class="bgclr">
 			<div id="page-wrap">
-				<h1 class="title">Not so fast.</h1>
-				<h1 class="title">ಠ╭╮ಠ</h1>
+				<h1 class="title">This is akward...</h1>
+				<h1 class="title">ಠ_ಠ</h1>
 			<br>
 			<br>
-				<p class="text">Looks like you are trying to enter a page that isn't accessable for you.</p>
+				<p class="text">Looks like you are trying to enter a page that doesn't exist.</p>
 			<br>
 			<br>
 				<button>
