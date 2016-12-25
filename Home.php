f0a85d500af53bda624f1f3122d52ae4086d778c
JurrijnP <slubadog@gmail.com>
2016-12-25 20:42:52 +0100

    Created Git

---
+++ Home.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+
+	<head>
+		<title>A.R.S. Echo Creator</title>
+		<link rel="icon" href="https://cdn.discordapp.com/app-icons/249891250117804032/8006c62af129d35355732de574da495c.jpg" type="image/x-icon">
+	</head>
+	
+	<body>
+		<?php
+			echo "Hi"
+		?>
+	</body>
+</html>
